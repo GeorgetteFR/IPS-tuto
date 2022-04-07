ff7e84492c7e88e475981791ffc4934be1f97200
ElliotJacquetF <100349345+ElliotJacquetF@users.noreply.github.com>
2022-04-07 13:09:28 +0200

    Added the PlayerScore script

---
+++ CS213/Assets/Scripts/Core/Behaviors/PlayerScore.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void incrementScore()
+    {
+        score++;
+    }
+    void decrementScore()
+    {
+        score--;
+    }
+}
